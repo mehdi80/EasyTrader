ebc920a5cac56484e98de0d25ab1f465616aded5
mehdi80 <mk2954751@gmail.com>
2024-08-20 09:57:51 +0330

    add: add a service for using to hide and show components when clicked on their buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet} from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
 import {SearchComponent} from "./components/search/search.component";
+import {NgIf} from "@angular/common";
+import {BtnHideShowVisibility} from "./services/btn-hide-show-visibility";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, SearchComponent],
+  imports: [RouterOutlet, MenuComponent, SearchComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Easy-Trader';
+
+  isSearchVisible = false;
+
+  constructor(private btnHideShowVisibility: BtnHideShowVisibility) {}
+
+  ngOnInit() {
+    this.btnHideShowVisibility.getVisibility('search').subscribe(visible => {
+      this.isSearchVisible = visible;
+    });
+  }
 }
